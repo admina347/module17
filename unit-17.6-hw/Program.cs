74d06ecb8b0944abca9de69532e7f3fe26183a34
admina <it-mactep@mail.ru>
2022-11-19 14:34:14 +0500

    Liskov

--- unit-17.6-hw/Program.cs
+++ unit-17.6-hw/Program.cs
@@ -1,6 +1,13 @@
 ﻿namespace Unit_17_6;
-public class Account
+public abstract class Account
 {
+    protected Account(string type, double balance, double interest)
+    {
+        Type = type;
+        Balance = balance;
+        Interest = interest;
+    }
+
     // тип учетной записи
     public string Type { get; set; }
 
@@ -9,22 +16,60 @@ public class Account
 
     // процентная ставка
     public double Interest { get; set; }
+
+    public void CalculateInterest(ICalculator calculator)
+    {
+        calculator.CalculateInterest(this);
+    }
+    // Подсчет количества пассажиров
+    public void CountPassengers()
+    {
+        Console.WriteLine($"Аккаунт {Type} Баланс {Balance} ставка {Interest}");
+    }
 }
+
+/// <summary>
+/// Интерфейс с общим функционалом
+/// </summary>
+public interface ICalculator
+{
+    void CalculateInterest(Account account);
+}
+
+
+
 
 public class BaseAccount : Account
 {
+    public BaseAccount(string type, double balance, double interest) : base(type, balance, interest)
+    {
+    }
 
+    public void CalculateInterest(Account account)
+    {
+        Console.WriteLine("Aккаунт:" + account.Type);
+    }
 }
 
 public class SalaryAccount : Account
 {
+    public SalaryAccount(string type, double balance, double interest) : base(type, balance, interest)
+    {
+    }
 
+    public void CalculateInterest(Account account)
+    {
+        Console.WriteLine("Аккаунт:" + account.Type);
+    }
 }
 
-public static class Calculator
+
+
+
+/* public class Calculator : ICalculator
 {
     // Метод для расчета процентной ставки
-    public static void CalculateInterest(Account account)
+    public void CalculateInterest(Account account)
     {
         if (account.Type == "Обычный")
         {
@@ -42,28 +87,55 @@ public static class Calculator
             // расчет процентной ставк зарплатного аккаунта по правилам банка
             account.Interest = account.Balance * 0.5;
         }
-        Console.WriteLine("Расчет: "+account.Interest);
+        Console.WriteLine("Расчет: " + account.Interest);
     }
-}
+} */
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Новый список полётов
-        var account = new Account();
-        account.Type = "Зарплатный";
-        account.Balance = 25000;
-        account.Interest = 0.2;
+        // Новый список аккаунтов
+        var accountList = new List<Account>()
+        {
+            new BaseAccount("Обычный", 5000, 0),
+            new BaseAccount("Обычный", 10000, 0),
+            new BaseAccount("Обычный", 15000, 0),
+            new SalaryAccount("Зарплатный", 15000, 0),
+            new SalaryAccount("Зарплатный", 20000, 0),
+            new SalaryAccount("Зарплатный", 25000, 0)
+        };
+        // Считаем ставки
+        FlightPassengerCount(accountList);
 
-        Calculator.CalculateInterest(account);
-/*         var flightsList = new List<Flight>()
-   {
-       new DomesticFlight("Mow-32", new List<string>() {"Вася", "Петя"}),
-       new DomesticFlight("SPB-14", new List<string>() {"Андрей"})
-   }; */
+        //ICalculator calculator = new Calcultor();
+        //Account ac = new Account();
+        //ac.Type = "необычный";
+        //ac.CalculateInterest(calculator);
+        //
+        //calculator.CalculateInterest();
+        //  Document doc = new Document();
+        //  doc.Text = "Hello World";
+        //  doc.Export(exporter);
+
+
+        //account.CalculateInterest(account);
+
+        /*         var flightsList = new List<Flight>()
+           {
+               new DomesticFlight("Mow-32", new List<string>() {"Вася", "Петя"}),
+               new DomesticFlight("SPB-14", new List<string>() {"Андрей"})
+           }; */
 
         // Считаем пассажиров
         //FlightPassengerCount(flightsList);
     }
+    /// <summary>
+        /// Метод для подсчёта количества пассажиров
+        /// </summary>
+        public static void FlightPassengerCount(List<Account> accountList)
+        {
+            foreach (var account in accountList)
+               account.CountPassengers();
+        }
 }
